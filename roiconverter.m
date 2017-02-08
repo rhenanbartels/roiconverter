736783694531d564c8e7892935c2bc00e2f299e8
Rhenan Bartels <rhenan.bartels@gmail.com>
2017-02-08 17:03:46 -0200

    Create Graphical User Interface

--- roiconverter.m
+++ roiconverter.m
@@ -1,18 +1,24 @@
-function roiconverter(folderPath, ext)
+function roiconverter()
 
-    if nargin == 1
-        ext = '.nrrd';
-    end
-    
-    if ~strcmp(ext, '.nrrd')
-        error('This version only supports NRRD extension.');
-    end
-    
     initDependencies();
 
+    createInterface();
+    
+    
+
+    
+end
+
+function convert(hObject, eventdata)
+    handles = guidata(hObject);
+    
+    folderPath = handles.data.folderPath;
+
+    logTextArea(handles.textArea, 'Verarbeiten...');
     [metadatas, img] = opendicoms(folderPath);
     
     if ~isempty(metadatas) && ~isempty(img)
+        ext = '.nrrd';
         
         lungRoi = getroiinfo(metadatas);
         
@@ -28,10 +34,96 @@ function roiconverter(folderPath, ext)
         if any(sum(sum(lungRoi)))
             matrix2nrrd(roiFileName, lungRoi, metadatas{1})
         end
-    else
-        disp('No DICOMS file found!');
-    end
+        
+    logTextArea(handles.textArea, ['DICOMS in ' resultsFolder ' gespeichert!']);    
     
+    else
+        logTextArea(handles.textArea, 'DICOMS nicht gefunden');
+    end
+end
+
+function logTextArea(handle, msg, flush)
+    if nargin < 3
+        flush = 0;
+    end
+    if flush
+        set(handle, 'String', '')
+    else
+        set(handle, 'String', msg);
+       drawnow
+    end
+end
+
+
+function getRootFolder(hObject, eventdata)
+    folderPath = uigetdir('Wählen Sie einen Ordner ');
+    if folderPath
+        handles = guidata(hObject);
+        set(handles.txtRootFolder, 'String', folderPath);
+        set(handles.btnDoIt, 'Enable', 'On')
+        handles.data.folderPath = folderPath;
+        guidata(hObject, handles);
+    end
+end
+
+function createInterface()
+    fig = figure('Menubar', 'None',...
+        'Name', 'ROI Converter',...
+        'NumberTitle', 'Off');
+    figPosition = get(fig, 'Position');
+    figPosition(3) = figPosition(1) * 1.6;
+    figPosition(4) = figPosition(2) * 0.7;
+    set(fig, 'Position', figPosition);
+    bckColor = get(fig, 'Color');
+    
+    uicontrol('Parent', fig,...
+        'String', 'Ordner wählen',...
+        'Units', 'Normalized',...
+        'Position', [0.01, 0.6, 0.165, 0.2],...
+        'Callback', @getRootFolder);
+    uicontrol('Parent', fig,...
+        'String', '',...
+        'Style', 'Edit',...
+        'Units', 'Normalized',...
+        'Position', [0.18, 0.6, 0.8, 0.2],...
+        'Backgroundcolor', [1 1 1],...
+        'Tag', 'txtRootFolder')
+    
+    uicontrol('Parent', fig,...
+        'String', 'Hauptordner',...
+        'Style', 'Text',...
+        'Units', 'Normalized',...
+        'Position', [0.43, 0.82, 0.2, 0.15],...
+        'Backgroundcolor', bckColor,...
+        'Fontweight', 'bold')
+    
+    
+    uicontrol('Parent', fig,...
+        'String', 'Auf geht''s',...
+        'Units', 'Normalized',...
+        'Position', [0.46, 0.32, 0.12, 0.25],...
+        'Enable', 'Off',...
+        'Callback', @convert,...
+        'Tag', 'btnDoIt');
+        
+    uicontrol('Parent', fig,...
+        'String', 'Log:',...
+        'Style', 'Text',...
+        'Units', 'Normalized',...
+        'Position', [0.025, 0.02, 0.98, 0.3],...
+        'Backgroundcolor', bckColor,...
+        'HorizontalAlignment', 'Left',...
+        'Tag', 'textArea')   
+
+    
+    handles = guihandles(fig);
+    
+    handles.hyper = [-1000, -901];
+    handles.normally = [-901, -501];
+    handles.poorly = [-500, -101];
+    handles.non = [-100, 100];
+    
+    guidata(fig, handles);
 end
 
 function [resultsFolder, imgFileName, roiFileName] =...
@@ -48,6 +140,7 @@ function [resultsFolder, imgFileName, roiFileName] =...
     roiFileName = [resultsFolder filesep 'roi' ext];
     
 end
+
 
 function initDependencies()
     addpath('external/');
